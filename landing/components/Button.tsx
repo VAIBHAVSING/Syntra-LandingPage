aa5ed1d9f9b3b5c5a25ea84d0f0070a60beed616
Vaibhav Patil <vpatil5212@gmail.com>
2024-10-05 15:16:52 +0530

    second commit

--- landing/components/Button.tsx
+++ landing/components/Button.tsx
@@ -2,21 +2,21 @@
 import Link from 'next/link';
 import { redirect } from 'next/navigation'
 
-interface ButtonInterface{
-    text:string
+interface ButtonInterface {
+  text: string
 }
-export default function Button({text}:ButtonInterface) {
-  const link='https://calendly.com/ayush-syntra/30min';
-  
-    return (
-      <div>
-        <Link className="p-[3px] relative" href={link} >
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-          <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
-            {text}
-          </div>
-        </Link>
-      </div>
-    );
-  }
-  
+export default function Button({ text }: ButtonInterface) {
+  const link = 'https://calendly.com/ayush-syntra/30min';
+
+  return (
+    <div>
+      <button className="p-[3px] relative"onClick={()=>{window.location.href=link}} >
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+        <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+          {text}
+        </div>
+      </button>
+    </div>
+  );
+}
+
